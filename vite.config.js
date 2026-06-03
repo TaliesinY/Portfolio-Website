53a07bd151b40e51f5213d9ebea8917492d4e423
TaliesinY <taliesinyip@gmail.com>
2026-06-03 18:48:32 -0400

    UI changes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio-Website/'
+  base: '/Portfolio-Website/',
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name]-[hash]-${Date.now()}.js`,
+        chunkFileNames: `assets/[name]-[hash]-${Date.now()}.js`,
+      }
+    }
+  }
 })
